7da227c9a5988e129f61dc6f892eb73d03ea2f3d
unknown <FGCARVA@NB442072.corp.ad.emb>
2017-02-20 15:27:47 -0300

    update angular app

--- src/main/resources/template/appmodule.js
+++ src/main/resources/template/appmodule.js
@@ -1,7 +1,14 @@
 var app = angular.module('dish-app', []);
 app.controller('controller', function($scope, $http){
-	$http.get('http://localhost:9090/dish/2/json').
-	then(function(response){
-		$scope.dish = response.data;
-	});
+	url = '';
+	$scope.findId = function(id){
+		$scope.dishId = id;
+		$scope.url = 'http://localhost:9090/dish/' + $scope.dishId + '/json';
+		$http.get($scope.url).
+		then(function(response){
+			$scope.dish = response.data;
+		});
+	}
+	
+	
 });
